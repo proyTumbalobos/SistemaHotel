8f69473978defbbab362af2d9b0575f3e37fd5a6
proyTumbalobos <mctumbalobost@ucvvirtual.edu.pe>
2024-12-04 15:46:07 -0500

    perfil

--- routes/web.php
+++ routes/web.php
@@ -34,9 +34,6 @@ Route::get('/sitio', function () {
 
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
     Route::resource('/cuarto', CuartoController::class);
     Route::resource('/categoria', CategoriaController::class);
     Route::resource('/piso', PisoController::class);
