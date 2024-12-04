3fa2cb3b7573184e6a08cf417bc5259475555966
proyTumbalobos <mctumbalobost@ucvvirtual.edu.pe>
2024-12-04 02:06:32 -0500

    Corregir

--- config/database.php
+++ config/database.php
@@ -44,12 +44,12 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'url' => env('DB_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
+            'url' => env('DB_URL','mysql://root:VsjOiJZmsZjCAngUndObTXGqpVkiesMs@autorack.proxy.rlwy.net:28118/railway'),
+            'host' => env('DB_HOST', 'mysql.railway.internal'),
             'port' => env('DB_PORT', '3306'),
-            'database' => env('DB_DATABASE', 'laravel'),
+            'database' => env('DB_DATABASE', 'railway'),
             'username' => env('DB_USERNAME', 'root'),
-            'password' => env('DB_PASSWORD', ''),
+            'password' => env('DB_PASSWORD', 'VsjOiJZmsZjCAngUndObTXGqpVkiesMs'),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => env('DB_CHARSET', 'utf8mb4'),
             'collation' => env('DB_COLLATION', 'utf8mb4_unicode_ci'),
